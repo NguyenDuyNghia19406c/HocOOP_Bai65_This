421a6b99124dc8ad9e961533df09c0e8914acd0f
ASUS <ASUS@192.168.1.3>
2020-02-23 16:13:15 +0700

    HocOOP_This

---
+++ HocOOP_Bai65_This/src/NguyenDuyNghia/com/test/testSanPham.java
@@ -0,0 +1,17 @@
+package NguyenDuyNghia.com.test;
+
+import NguyenDuyNghia.com.model.SanPham;
+
+public class testSanPham {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SanPham sp1=new SanPham();
+		sp1.setMa(1);
+		sp1.setTen("Cocacola");
+		sp1.setGia(15);
+		System.out.println(sp1);
+
+	}
+
+}
